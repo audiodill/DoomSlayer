5809a53b0447fe39457aae8e50b602e866b81d2a
Jesse Dill <jesse.dill.sound@gmail.com>
2017-10-24 11:27:51 -0400

    first commit

---
+++ Project.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Project.Web.DAL;
+using Project.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPlayerDAL dal;
+
+        public HomeController(IPlayerDAL PlayerDal)
+        {
+            this.dal = PlayerDal;
+        }
+        // GET: Home
+        public ActionResult Home()
+        {
+            PlayerModel model = new PlayerModel();
+            return View("Home", model);
+        }
+
+        public ActionResult CreatePlayer()
+        {
+            return View("CreatePlayer");
+        }
+
+        [HttpPost]
+        public ActionResult CreatePlayer(PlayerModel player)
+        {
+            PlayerModel model = player;
+            string name = dal.CreateNewPlayer(model);
+            return RedirectToAction("PlayerHome", name);
+        }
+
+        public ActionResult PlayerHome(string name)
+        {
+            PlayerModel model = dal.GetPlayer(name);
+            return View("PlayerHome", model);
+        }
+    }
+}
---
+++ Project.Web/DAL/StoreSQL_DAL.cs
@@ -0,0 +1,58 @@
+﻿using Project.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project.Web.DAL
+{
+    public class StoreSQL_DAL : IStoreDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DoomSlayer"].ConnectionString;
+        private const string SQL_GetStoreInventory = "SELECT * FROM item";
+        private const string SQL_SortInventory = "SELECT * FROM item WHERE type = @category_id";
+        public List<StoreModel> GetStoreInventory(int sort)
+        {
+            List<StoreModel> output = new List<StoreModel>();
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd;
+                    if(sort == 0)
+                    {
+                        cmd = new SqlCommand(SQL_GetStoreInventory, conn);
+                    }
+                    else
+                    {
+                        cmd = new SqlCommand(SQL_SortInventory, conn);
+                        cmd.Parameters.AddWithValue("@category_id", sort);
+                    }
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        StoreModel s = new StoreModel();
+
+                        s.Name = Convert.ToString(reader["name"]);
+                        s.Price = Convert.ToInt32(reader["price"]);
+                        s.Weight = Convert.ToDecimal(reader["weight"]);
+                        s.Description = Convert.ToString(reader["description"]);
+                        s.ImageName = Convert.ToString(reader["image"]);
+
+                        output.Add(s);
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                throw;
+            }
+            return output;
+        }
+    }
+}
